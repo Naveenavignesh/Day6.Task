26231ec4057054dd128c97bd6dba1caa5885f81b
Naveenavignesh <naveenanavii4437@gmail.com>
2024-05-24 05:16:51 -0700

    movie,circles

---
+++ js/scripts.js
@@ -0,0 +1,21 @@
+class Person {
+
+    constructor(name, age, occupation) {
+    
+    this.name = name;
+    
+    this.age = age;
+    
+    this.occupation = occupation;
+    
+    }
+    
+    }
+    
+    const person = new Person("reena", 24, "Student”);
+    
+    console.log("Name:", person.name);
+    
+    console.log("Age:", person.age);
+    
+    console.log("Occupation:”, person.occupation)
